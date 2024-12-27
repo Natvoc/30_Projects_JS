4a89b33afce17a0242c794733f14633c1c9c6a1c
Natyari <naryari@gmail.com>
2024-12-27 17:18:30 -0600

    Solución Final

---
+++ proyeto1/script.js
@@ -0,0 +1,26 @@
+
+
+const counter = document.getElementById('counter');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const resetButton = document.getElementById('reset');
+
+let count = 0;
+
+incrementButton.addEventListener('click', () => {
+    count++;
+    counter.textContent = count;
+});
+
+decrementButton.addEventListener('click', () => {
+    if (count > 0){
+        count--;
+        counter.textContent = count;
+    }
+    
+});
+
+resetButton.addEventListener('click', () => {
+    count = 0
+    counter.textContent = count;
+});
